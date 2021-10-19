f6d19428c9d08266de40b2927c47e1ab5ca3fdd4
tradov00 <92442649+tradov00@users.noreply.github.com>
2021-10-19 12:13:51 +0200

    Add files via upload

---
+++ zad1.c
@@ -0,0 +1,104 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define IME 100
+#define MAX_LINE 1024
+
+
+typedef struct                                                              
+{
+  int ocjene;
+  char ime[IME];
+  char prezime[IME];
+}Student;
+
+
+
+int procitajDatoteku(char *imeDatoteke);                                    
+Student* alocirajIProcitajIzDatoteke(char* imeDatoteke, int brojStudenata); 
+int maxBodovi(Student *studenti,int brojStudenata);                             
+
+
+
+int main()                                                                 
+{
+    int brojRedaka;
+    int i=0;
+    Student *studenti;
+    char *imeDat = "datoteka.txt";
+    brojRedaka = procitajDatoteku(imeDat);
+    printf("Ova datoteka ima %d redaka.\n",brojRedaka);
+   
+    studenti = alocirajIProcitajIzDatoteke(imeDat, brojRedaka);
+    int max=maxBodovi(studenti,brojRedaka);
+    printf("IME PREZIME APS_BOD REL_BOD \n");
+    for(i=0;i<brojRedaka;i++)
+    {
+        printf("%s %s %d %.0lf\n",studenti[i].ime,studenti[i].prezime,studenti[i].ocjene,100*(double)studenti[i].ocjene/(double)max);
+    }
+   
+    return 0;
+}
+
+
+
+int procitajDatoteku(char *imeDatoteke)                                     
+{
+    int br=0;
+    FILE *fp=NULL;
+    char buffer[MAX_LINE]={0};
+   
+    fp=fopen(imeDatoteke,"r");
+    if(!fp)
+    {
+        printf("Neuspjesno otvaranje datoteke.\n");
+        return -1;
+    }
+   
+    while(!feof(fp))
+    {
+        fgets(buffer,MAX_LINE,fp);
+        br++;
+    }
+    fclose(fp);
+    return br;
+}
+
+
+
+Student *alocirajIProcitajIzDatoteke(char *imeDatoteke,int brojStudenata)   
+{
+    Student *studenti = NULL;
+    int i=0;
+    FILE *fp=NULL;
+    studenti = (Student *)malloc(sizeof(Student)*brojStudenata);
+    if(!studenti)
+    {
+        printf("Neuspjesna alokacija memorije.\n");
+        return NULL;
+    }
+    fp = fopen(imeDatoteke,"r");
+    if(!fp)
+    {
+        printf("Neuspjesno otvaranje datoteke.\n");
+        return NULL;
+    }
+    while(!feof(fp))
+    {
+        fscanf(fp," %s %s %d\n",studenti[i].ime,studenti[i].prezime,&studenti[i].ocjene);
+        i++;
+    }
+    fclose(fp);
+    return studenti;
+}
+
+
+
+int maxBodovi(Student *studenti,int brojStudenata)                              
+{
+    int i=0,max=0;
+    for(i=0;i<brojStudenata;i++)
+    {
+        if(studenti[i].ocjene > max)max=studenti[i].ocjene;    
+    }
+    return max;
+}
